629abcb7caf00f370c9bd04a3ae7441128588c92
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2015-09-16 16:05:26 +0200

    fix: add protobuf library to the mix

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 /* eslint quotes: [0], strict: [0] */
 var {
-    $d, $o, $f, _
+    $d, $o, $f, $b
 } = require('zaccaria-cli')
 
 var {
     go, chan, put, take
 } = require('js-csp')
 
+var pb = require('protobufjs')
+pb = $b.promisifyAll(pb)
+
 var debug = require('debug')('index')
 
-// var util = require('util')
-// var hex = require('hex')
+// var util    = require('util')
+var hex = require('hex')
 
 var serialport = require("serialport")
+
+var ByteBuffer = require("bytebuffer")
 
 var SerialPort = serialport.SerialPort
 
@@ -30,33 +35,48 @@ var getOptions = doc => {
 
 function* readSize(chan) {
     var s = 0
-    s = s * 256 + (yield take(chan)).data
-    s = s * 256 + (yield take(chan)).data
-    s = s * 256 + (yield take(chan)).data
-    s = s * 256 + (yield take(chan)).data
+    var vec = [0, 0, 0, 0];
+    vec[0] = (yield take(chan)).data;
+    vec[1] = (yield take(chan)).data;
+    vec[2] = (yield take(chan)).data;
+    vec[3] = (yield take(chan)).data;
+    debug("vec = " + JSON.stringify(vec));
+    s = s * 256 + vec[3]
+    s = s * 256 + vec[2]
+    s = s * 256 + vec[1]
+    s = s * 256 + vec[0]
     debug(`computed ${s}`)
     return s
 }
 
-function* dataConsumer(chan) {
+function* dataConsumer(chan, builder) {
     debug(`dataConsumer started`);
-    var prev = yield take(chan)
-    var cur = yield take(chan)
+    var pct = builder.build("pct")
     for (;;) {
+        var prev = yield take(chan)
+        var cur = yield take(chan)
+        debug('first two character received')
+
         while (cur.data !== 90 || prev.data !== 86) {
             debug(JSON.stringify(cur, 0, 4));
             prev = cur;
             cur = yield take(chan)
         }
-        var s = yield* readSize(chan)
+        var s = yield * readSize(chan)
         debug(`read header size ${s}`)
-        process.exit(0)
+        var buf = new ByteBuffer(s)
+        for (var i = 0; i < s; i++) {
+            var read = (yield take(chan)).data;
+            buf.writeByte(read, i)
+        }
+        var msg = pct.decode(buf)
+        debug("msg = " + JSON.stringify(msg));
     }
 }
 
 
 
-function* putDataOnChan(chan, data) {
+function* putDataOnChan(chan, data, builder) {
     for (var i = 0; i < data.length; i++) {
         yield put(chan, {
             data: data[i]
@@ -70,36 +90,40 @@ function registerSerialListener(port, rate, f) {
         parser: serialport.parsers.raw
     }, false);
     serialPort.open(() => {
-        serialPort.on('data', f)
+        serialPort.on('data', (data) => {
+            f(data)
+        })
     })
 }
 
-function startCSP() {
+function startCSP(builder) {
     var innerChan = chan()
-    go(dataConsumer, [innerChan])
+    go(dataConsumer, [innerChan, builder])
     return innerChan;
 }
 
 var main = () => {
     $f.readLocal('docs/usage.md').then(it => {
         var {
-            help, port, test
+            help, port, test, proto
         } = getOptions(it);
         if (help) {
             console.log(it)
         } else {
-            var innerChan = startCSP();
-            if (!test) {
-                registerSerialListener(port, 9600, (data) => {
-                    go(putDataOnChan, [innerChan, data])
-                })
-            } else {
-                //                       V   Z  s1 s2 s3 s4  payload
-                var tstData = [0, 0, 0, 86, 90, 0, 0, 0, 3, 11, 12, 13]
-                var buf = new Buffer(tstData);
-                debug(`Start test`);
-                go(putDataOnChan, [innerChan, buf]);
-            }
+            pb.loadProtoFileAsync(proto).then((builder) => {
+                var innerChan = startCSP(builder);
+                if (!test) {
+                    registerSerialListener(port, 9600, (data) => {
+                        go(putDataOnChan, [innerChan, data])
+                    })
+                } else {
+                    //                       V   Z  s1 s2 s3 s4  payload
+                    var tstData = [0, 0, 0, 86, 90, 0, 0, 0, 3, 11, 12, 13]
+                    var buf = new Buffer(tstData);
+                    debug(`Start test`);
+                    go(putDataOnChan, [innerChan, buf]);
+                }
+            })
         }
     })
 }
