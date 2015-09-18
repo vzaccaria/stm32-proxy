2f21fab89768fa8a432d42172b95c3e2ea56a6d9
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2015-09-18 16:18:23 +0200

    feat: add processing plugins

--- docs/usage.md
+++ docs/usage.md
@@ -1,11 +1,12 @@
 Usage:
-    stm32-proxy PROTO [ -p PORT ] [ -t ]
+    stm32-proxy PROTO [ -p PORT ] [ -g PLUGIN ]
     stm32-proxy ( -h | --help )
 
 Options:
     -h, --help              help for stm32-proxy
     -p, --port PORT         port to use to connect to the stm32
-    -t, --test              sanity check without connecting to port
+    -g, --plugin PLUGIN     javascript plugin (implements init() and process(data))
+
 
 Arguments:
     PROTO                   prototype file
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /* eslint quotes: [0], strict: [0] */
 var {
-    $d, $o, $f, $b
+    $d, $o, $f, $b, _
 } = require('zaccaria-cli')
 
 var {
@@ -13,7 +13,6 @@ pb = $b.promisifyAll(pb)
 var debug = require('debug')('index')
 
 // var util    = require('util')
-var hex = require('hex')
 
 var serialport = require("serialport")
 
@@ -28,8 +27,9 @@ var getOptions = doc => {
     var proto = o.PROTO
     var port = $o('-p', '--port', '/dev/cu.usbmodemfd123', o)
     var test = $o('-t', '--test', false, o)
+    var plugin = $o('-g', '--plugin', undefined, o)
     return {
-        help, port, proto, test
+        help, port, proto, test, plugin
     }
 }
 
@@ -49,7 +49,7 @@ function* readSize(chan) {
     return s
 }
 
-function* dataConsumer(chan, builder) {
+function* dataConsumer(chan, builder, plugin) {
     debug(`dataConsumer started`);
     var pct = builder.build("pct")
     for (;;) {
@@ -70,13 +70,16 @@ function* dataConsumer(chan, builder) {
             buf.writeByte(read, i)
         }
         var msg = pct.decode(buf)
+        if(!_.isUndefined(plugin)) {
+            plugin.process(msg)
+        }
         debug("msg = " + JSON.stringify(msg));
     }
 }
 
 
 
-function* putDataOnChan(chan, data, builder) {
+function* putDataOnChan(chan, data) {
     for (var i = 0; i < data.length; i++) {
         yield put(chan, {
             data: data[i]
@@ -96,22 +99,28 @@ function registerSerialListener(port, rate, f) {
     })
 }
 
-function startCSP(builder) {
+function startCSP(builder, plugin) {
     var innerChan = chan()
-    go(dataConsumer, [innerChan, builder])
+    if(!_.isUndefined(plugin)) {
+        plugin.init()
+    }
+    go(dataConsumer, [innerChan, builder, plugin])
     return innerChan;
 }
 
 var main = () => {
     $f.readLocal('docs/usage.md').then(it => {
         var {
-            help, port, test, proto
+            help, port, test, proto, plugin
         } = getOptions(it);
         if (help) {
             console.log(it)
         } else {
             pb.loadProtoFileAsync(proto).then((builder) => {
-                var innerChan = startCSP(builder);
+                if(!_.isUndefined(plugin)) {
+                    plugin = require(plugin)
+                }
+                var innerChan = startCSP(builder, plugin);
                 if (!test) {
                     registerSerialListener(port, 9600, (data) => {
                         go(putDataOnChan, [innerChan, data])
